3b2706b931c31bc62f7bc976941ba29f897f99c8
Steven <steven.a.pinkerton@gmail.com.com>
2022-10-31 16:20:12 +0000

    WIP Branched From master + Implementation

--- src/Main.hs
+++ src/Main.hs
@@ -2,13 +2,102 @@ module Main where
 
 import Main.Utf8 qualified as Utf8
 
-{- |
- Main entry point.
+import Data.Text as T (lines, unlines)
+import Data.Text.Encoding as TE (decodeUtf8)
+import Data.Time (UTCTime, defaultTimeLocale, parseTimeOrError)
+import Network.HTTP.Types (status200)
+import Network.Wai (Application, responseLBS)
+import Network.Wai.Handler.Warp (run)
+import Relude qualified
+import Relude.List.Reexport ()
+import Text.Blaze.Html.Renderer.Utf8 (renderHtml)
+import Text.Blaze.Html5 qualified as H
+import Text.Blaze.Html5.Attributes (charset)
 
- The `bin/run` script will invoke this function.
--}
+data Mood = Bad | Netural | Good | Great | Excellent
+  deriving stock (Show)
+
+data MoodEntry = MoodEntry
+  { moodWhen :: UTCTime
+  , moodWhat :: Mood
+  }
+  deriving stock (Show)
+
+fileToByte :: MonadIO m => FilePath -> m ByteString
+fileToByte = Relude.readFileBS
+
+byteToText :: ByteString -> Text
+byteToText = TE.decodeUtf8
+
+filterDays :: Text -> Text
+filterDays x =
+  Relude.unlines $ Relude.filter (\w -> w `notElem` ["[", "]", "Mon", "Thu", "Wen", "Thr", "Fri", "Sat", "Sun"]) (T.lines x)
+
+filterMoods :: Text -> Text
+filterMoods x =
+  T.unlines $ Relude.filter (\w -> w `Prelude.elem` ["Good", "Neutral", "Bad", "Great", "Excellent"]) (T.lines x)
+
+moodChecker :: Text -> Mood
+moodChecker x
+  | x == "Good" = Good
+  | x == "Nutral" = Netural
+  | x == "Bad" = Bad
+  | x == "Excellent" = Excellent
+  | otherwise = error "Nothing has been provided"
+
+moodList :: [Text] -> [Mood]
+moodList = fmap moodChecker
+
+timeList :: Text -> UTCTime
+timeList x =
+  let filteredList = show (Relude.filter (\w -> w `notElem` ["good", "netural", "bad"]) [x])
+   in parseTimeOrError True defaultTimeLocale "%F %H:%M" filteredList :: UTCTime
+
+moodParse :: (UTCTime, Mood) -> MoodEntry
+moodParse (x, y) = MoodEntry x y
+
+moodParser :: Text -> [MoodEntry]
+moodParser x =
+  let targetList = words x
+      time = fmap timeList targetList
+      moods = fmap filterMoods targetList
+      mList = moodList moods
+      ziped = zip time mList
+   in fmap feederToParser ziped
+
+feederToParser :: (UTCTime, Mood) -> MoodEntry
+feederToParser x =
+  let time = fst x
+      mood = snd x
+   in MoodEntry time mood
+
+-- | This serves Html to an application.
+renderMoods :: [MoodEntry] -> H.Html
+renderMoods persons =
+  H.docTypeHtml $ do
+    H.body $ do
+      H.h1 "Hello"
+      H.meta H.! charset "UTF-8"
+      H.ul $ do
+        mapM_ H.li $ fmap renderMood persons
+
+renderMood :: MoodEntry -> H.Html
+renderMood = show . moodWhat
+
+-- | This runs a web application, at the given port.
+runApp :: Int -> IO ()
+runApp port = do
+  Prelude.putStrLn $ "Running HTTP server at http://127.0.0.1:" <> show port
+  run port app
+
+-- | This creates a web-server.
+app :: Application
+app _request respond = do
+  targetFile <- fileToByte "src/moods.txt"
+  let moodlist = moodParser $ byteToText targetFile
+      response = renderHtml $ renderMoods moodlist
+  respond $ responseLBS status200 [] response
+
 main :: IO ()
 main = do
-  -- For withUtf8, see https://serokell.io/blog/haskell-with-utf8
-  Utf8.withUtf8 $ do
-    putTextLn "Hello 🌎"
+  runApp 3000
