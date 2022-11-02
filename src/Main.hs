91e6a24d521796f461696a4bb6ba1b3cc15932f2
Steven <steven.a.pinkerton@gmail.com.com>
2022-11-02 19:12:43 +0000

    Moods.JSonl and Refactor of main

--- src/Main.hs
+++ src/Main.hs
@@ -4,26 +4,21 @@ import Data.Time (UTCTime, defaultTimeLocale, parseTimeOrError)
 import Network.HTTP.Types (status200)
 import Network.Wai (Application, responseLBS)
 import Network.Wai.Handler.Warp (run)
-import Relude qualified
 import Relude.List.Reexport ()
 import Text.Blaze.Html.Renderer.Utf8 (renderHtml)
 import Text.Blaze.Html5 qualified as H
 import Text.Blaze.Html5.Attributes (charset)
+import Data.Aeson qualified as DA
 
 data Mood = Bad | Netural | Good | Great | Excellent
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
 data MoodEntry = MoodEntry
   { moodWhen :: UTCTime
   , moodWhat :: Mood
   }
-  deriving stock (Show)
+  deriving stock (Show, Eq)
 
-fileToByte :: MonadIO m => FilePath -> m ByteString
-fileToByte = Relude.readFileBS
-
-byteToText :: ByteString -> Text
-byteToText = Relude.decodeUtf8
 
 {-
 filterDays :: Text -> Text
@@ -31,43 +26,43 @@ filterDays x =
   Relude.unlines $ Relude.filter (\w -> w `notElem` ["[", "]", "Mon", "Thu", "Wen", "Thr", "Fri", "Sat", "Sun"]) (Relude.lines x)
 -}
 
+
 filterMoods :: Text -> Text
 filterMoods x =
-  Relude.unlines $ Relude.filter (\w -> w `Prelude.elem` ["good", "neutral", "bad", "great", "excellent"]) (Relude.lines x)
+  unlines $ filter (\w -> w `elem` ["good", "neutral", "bad", "great", "excellent"]) (lines x)
 
-moodChecker :: Text -> Mood
-moodChecker x
+parseMood :: Text -> Mood
+parseMood x
   | x == "Good" = Good
   | x == "Nutral" = Netural
   | x == "Bad" = Bad
   | x == "Excellent" = Excellent
   | otherwise = error "Nothing has been provided"
 
-moodList :: [Text] -> [Mood]
-moodList = fmap moodChecker
-
 timeFilter' :: Text -> Text
 timeFilter' x =
-  Relude.unwords $ Relude.filter (\w -> w `notElem` ["good", "netural", "bad", "great", "excellent", "[", "]", "Mon", "Tus", "Wen", "Thu", "Fri", "Sat", "Sun"]) (words x)
+  unwords $ filter (\w -> w `notElem` ["good", "netural", "bad", "great", "excellent", "[", "]", "Mon", "Tus", "Wen", "Thu", "Fri", "Sat", "Sun"]) (words x)
 
 timeFilter :: Text -> [Text]
 timeFilter x =
-  Relude.filter (\w -> w `notElem` ["good", "netural", "bad", "great", "excellent", "[", "]", "Mon", "Tus", "Wen", "Thu", "Fri", "Sat", "Sun"]) (words x)
+  filter (\w -> w `notElem` ["good", "netural", "bad", "great", "excellent", "[", "]", "Mon", "Tus", "Wen", "Thu", "Fri", "Sat", "Sun"]) (words x)
 
 timeList :: Text -> UTCTime
 timeList x =
-  let filteredList = toString $ Relude.unlines $ timeFilter x
+  let filteredList = toString $ unlines $ timeFilter x
    in parseTimeOrError True defaultTimeLocale "%F %H:%M" filteredList :: UTCTime
 
+
+--Delete this in favour of uncurry.
 moodParse :: (UTCTime, Mood) -> MoodEntry
 moodParse (x, y) = MoodEntry x y
 
-moodParser :: Text -> [MoodEntry]
-moodParser x =
-  let targetList = lines x
-      time = fmap timeList targetList
-      moods = fmap filterMoods targetList
-      mList = moodList moods
+parseMoodEntries :: Text -> [MoodEntry]
+parseMoodEntries x =
+  let inputList = lines x
+      time = fmap timeList inputList
+      moods = fmap filterMoods inputList
+      mList = fmap parseMood moods
       ziped = zip time mList
    in fmap feederToParser ziped
 
@@ -99,8 +94,8 @@ runApp port = do
 -- | This creates a web-server.
 app :: Application
 app _request respond = do
-  targetFile <- fileToByte "src/moods.txt"
-  let moodlist = moodParser $ byteToText targetFile
+  targetFile <- readFileBS "src/moods.txt"
+  let moodlist = parseMoodEntries $ decodeUtf8 targetFile
       response = renderHtml $ renderMoods moodlist
   respond $ responseLBS status200 [] response
 
