5c8fd7044ee29f82ff898947bec516ca9581d2a4
Steven <steven.a.pinkerton@gmail.com.com>
2022-11-22 16:24:11 +0000

    Added test for multi-line file

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -1,7 +1,7 @@
 module Spec where
 
 import Data.Maybe (fromJust)
-import MoodTracker.Parser (Mood (Bad), MoodEntry (MoodEntry), moodParse, moodWhat, moodWhen, myParseTimeM)
+import MoodTracker.Parser (Mood (Bad, Good), MoodEntry (MoodEntry), moodParse, moodWhat, moodWhen, myParseTimeM)
 import Test.Hspec (describe, hspec, it, shouldBe)
 
 main :: IO ()
@@ -12,4 +12,4 @@ main = do
       it "moodParse works" $ do
         moodParse file
           `shouldBe` Right
-            [MoodEntry {moodWhen = fromJust $ myParseTimeM "2022-10-09 Mon 11:51", moodWhat = Bad}]
+            [MoodEntry {moodWhen = fromJust $ myParseTimeM "2022-10-09 Mon 11:51", moodWhat = Bad}, MoodEntry {moodWhen = fromJust $ myParseTimeM "2022-11-02 Sat 09:10", moodWhat = Good}]
