6a71d8d90bdb9aa22f17eb6f7a7f6b94d98de24d
Steven <steven.a.pinkerton@gmail.com.com>
2022-11-23 12:20:13 +0000

    Simplifed Tests

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -1,9 +1,19 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Spec where
 
-import Data.Maybe (fromJust)
 import Data.ByteString.Lazy ()
+import Data.Maybe (fromJust)
 import MoodTracker.Parser (Mood (Bad, Good), MoodEntry (MoodEntry), moodParse, moodWhat, moodWhen, myParseTimeM)
+import NeatInterpolation (trimming)
 import Test.Hspec (describe, hspec, it, shouldBe)
+
+--This provides the text for the test.
+testText :: Text
+testText =
+  [trimming|
+      {"mood": "Bad", "when": "2022-10-09 Mon 11:51"}
+      {"mood": "Good", "when": "2022-11-02 Sat 09:10"}|]
 
 main :: IO ()
 main = do
