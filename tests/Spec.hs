b0b6da31d3c8851f9fe86ab9a11de5063d4b9d7a
Sridhar Ratnakumar <srid@srid.ca>
2022-11-28 11:44:49 -0500

    Refactor test

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -8,18 +8,17 @@ import MoodTracker.Parser (Mood (Bad, Good), MoodEntry (MoodEntry), moodWhat, mo
 import NeatInterpolation (trimming)
 import Test.Hspec (describe, hspec, it, shouldBe)
 
---This provides the text for the test.
-testText :: Text
-testText =
-  [trimming|
-      {"mood": "Bad", "when": "2022-10-09 Mon 11:51"}
-      {"mood": "Good", "when": "2022-11-02 Sat 09:10"}|]
-
+-- This provides the text for the test.
 main :: IO ()
 main = do
   hspec $ do
     describe "Main" $ do
       it "moodParse works" $ do
+        let testText :: Text
+            testText =
+              [trimming|
+                  {"mood": "Bad", "when": "2022-10-09 Mon 11:51"}
+                  {"mood": "Good", "when": "2022-11-02 Sat 09:10"}|]
         parseMoodEntries (encodeUtf8 testText)
           `shouldBe` Right
             [ MoodEntry {moodWhen = fromJust $ parseTimeM False defaultTimeLocale "%Y-%m-%d %a %H:%M" "2022-10-09 Mon 11:51", moodWhat = Bad}
